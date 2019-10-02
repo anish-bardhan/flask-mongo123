328701d17bc7450c266ae58bda8f6ec7891e053b
Anish Bardhan <2020abardhan@dwight.edu>
2019-10-01 22:03:07 -0400

    updated

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ import os
 from app import app
 from flask import render_template, request, redirect
 
+# the purpose of this application is to allow a user to upload information about upcoming events.
+
 username = "benlee"
 
 events = [
@@ -10,7 +12,6 @@ events = [
         {"event":"Finals Begin", "date":"2019-12-01"},
         {"event":"Summer Vacation", "date":"2019-06-14"}
     ]
-
 
 from flask_pymongo import PyMongo
 
@@ -25,28 +26,35 @@ mongo = PyMongo(app)
 
 # INDEX
 
+#sets the homepage
 @app.route('/')
 @app.route('/index', methods= ['GET','POST'])
 
 def index():
+    #
     for_display = {}
     information = mongo.db.events
     queries = list(information.find({}))
     print(queries)
-
+    # prevent error before info is added
     if dict(request.form):
+        # retrieve data from form
         added_data = dict(request.form)
+        # find name
         event_name = added_data["event_name"]
+        # find date
         date =  added_data["date"]
+        # find attendance
         attending = added_data["attending"]
         event = mongo.db.events
+        # send all info
         event.insert({"name":event_name, "date": date, "attending": attending})
-
     return render_template('index.html', queries = queries)
 
 
 # CONNECT TO DB, ADD DATA
 
+#creates a page for the form
 @app.route('/input', methods= ['GET',"POST"])
 def input():
     return render_template('input.html')
